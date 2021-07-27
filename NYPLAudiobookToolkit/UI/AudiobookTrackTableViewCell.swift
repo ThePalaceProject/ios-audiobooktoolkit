f47bf6cddf00d69a770d36cfc55d34d7fbee57f2
mauricecarrier7 <Maurice.Carrier@outlook.com>
2021-07-27 12:18:55 -0400

    check availability before accessing api

--- NYPLAudiobookToolkit/UI/AudiobookTrackTableViewCell.swift
+++ NYPLAudiobookToolkit/UI/AudiobookTrackTableViewCell.swift
@@ -37,8 +37,11 @@ class AudiobookTrackTableViewCell: UITableViewCell {
         self.textLabel?.text = title
         self.textLabel?.alpha = labelAlpha
         self.detailTextLabel?.text = detailLabel
-        self.textLabel?.textColor = UIColor.label
-        self.detailTextLabel?.textColor = UIColor.secondaryLabel
-        self.backgroundColor = UIColor.systemBackground
+        
+        if #available(iOS 13.0, *) {
+            self.textLabel?.textColor = UIColor.label
+            self.detailTextLabel?.textColor = UIColor.secondaryLabel
+            self.backgroundColor = UIColor.systemBackground
+        }
     }
 }
