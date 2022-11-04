5c3df04f7d546b6ce5bbb311e17ae2e3e18981be
mauricecarrier7 <Maurice.Carrier@outlook.com>
2022-11-04 12:52:57 -0400

    Update AudiobookPlayerViewController.swift

--- NYPLAudiobookToolkit/UI/AudiobookPlayerViewController.swift
+++ NYPLAudiobookToolkit/UI/AudiobookPlayerViewController.swift
@@ -453,7 +453,7 @@ let SkipTimeInterval: Double = 15
             return
         }
         
-        guard currentLocation.duration > 0 else {
+        guard self.seekBar.state.progress.duration >= self.seekBar.state.progress.offset else {
             self.audiobookManager.audiobook.nextChapter()
             return
         }
