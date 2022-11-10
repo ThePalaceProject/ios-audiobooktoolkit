eba7567d5e4f11d22e4c838f7fdd03a496a777a8
mauricecarrier7 <Maurice.Carrier@outlook.com>
2022-11-10 10:04:49 -0500

    Update OpenAccessPlayer.swift

--- NYPLAudiobookToolkit/Player/OpenAccessPlayer.swift
+++ NYPLAudiobookToolkit/Player/OpenAccessPlayer.swift
@@ -74,7 +74,7 @@ class OpenAccessPlayer: NSObject, Player {
         let playerItemStatus = self.avQueuePlayer.currentItem?.status
         let offset: TimeInterval
         if !avPlayerOffset.isNaN && playerItemStatus == .readyToPlay {
-            offset = avPlayerOffset - self.chapterAtCurrentCursor.startOffset
+            offset = avPlayerOffset
         } else {
             offset = 0
         }
@@ -82,7 +82,7 @@ class OpenAccessPlayer: NSObject, Player {
             number: self.chapterAtCurrentCursor.number,
             part: self.chapterAtCurrentCursor.part,
             duration: self.chapterAtCurrentCursor.duration,
-            startOffset: offset,
+            startOffset: self.chapterAtCurrentCursor.startOffset,
             playheadOffset: offset,
             title: self.chapterAtCurrentCursor.title,
             audiobookID: self.audiobookID
