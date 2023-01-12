4984288a92a8b27f1db842342dcaed33e476fdd8
MauriceCarrier <Maurice.Carrier@outlook.com>
2023-01-12 16:34:27 -0500

    Update AudiobookPlayerViewController.swift

--- NYPLAudiobookToolkit/UI/AudiobookPlayerViewController.swift
+++ NYPLAudiobookToolkit/UI/AudiobookPlayerViewController.swift
@@ -595,6 +595,7 @@ extension AudiobookPlayerViewController: AudiobookManagerTimerDelegate {
 
 extension AudiobookPlayerViewController: PlaybackControlViewDelegate {
     func playbackControlViewSkipBackButtonWasTapped(_ playbackControlView: PlaybackControlView) {
+        guard !waitingForPlayer else { return }
 
         self.waitingForPlayer = true
         if self.audiobookManager.audiobook.player.isPlaying {
@@ -611,6 +612,7 @@ extension AudiobookPlayerViewController: PlaybackControlViewDelegate {
     }
     
     func playbackControlViewSkipForwardButtonWasTapped(_ playbackControlView: PlaybackControlView) {
+        guard !waitingForPlayer else { return }
 
         self.waitingForPlayer = true
         if self.audiobookManager.audiobook.player.isPlaying {
