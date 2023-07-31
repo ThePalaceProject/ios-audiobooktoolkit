dfa69939243b2aa4b865c84738939b05d3d85522
Vladimir Fedorov <fedorovvs@gmail.com>
2023-07-31 20:03:23 +0200

    Add code documentation

--- NYPLAudiobookToolkit/Tracker/AudiobookPlaybackTrackerDelegate.swift
+++ NYPLAudiobookToolkit/Tracker/AudiobookPlaybackTrackerDelegate.swift
@@ -8,8 +8,15 @@
 
 import Foundation
 
+/// Audiobook playback delegate for playback time tracking.
+///
+/// Audiobook player calls two functions to track its playback state:
+///  - `playbackStarted()` when the player starts playing a track of an audiobook
+///  - `playbackStopped()` when playbook is stopeed, e.g., when the player switches between chapters, the user paused the playback, etc.
 @objc
 public protocol AudiobookPlaybackTrackerDelegate {
+    /// Audiobook player stated playing a track.
     func playbackStarted()
+    /// Audiobook player paused or stopped playing a track..
     func playbackStopped()
 }
