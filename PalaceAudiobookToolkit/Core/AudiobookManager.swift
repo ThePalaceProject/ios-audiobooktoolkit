e834afdfd61c83b2260585348d1e3706a545aa3e
Maurice Carrier <maurice.carrier@outlook.com>
2024-09-13 15:46:43 -0400

    Update AudiobookManager.swift

--- PalaceAudiobookToolkit/Core/AudiobookManager.swift
+++ PalaceAudiobookToolkit/Core/AudiobookManager.swift
@@ -356,7 +356,6 @@ public final class DefaultAudiobookManager: NSObject, AudiobookManager {
     
     private func handlePlaybackCompleted(_ chapter: Chapter) {
         waitingForPlayer = false
-        playbackTrackerDelegate?.playbackStopped()
         statePublisher.send(.playbackStopped(chapter.position))
     }
     
